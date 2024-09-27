06273d48a9d092deffc68e70f3dada41b95fc9e5
CHANDRA SEKAR <157828207+Chandrurio@users.noreply.github.com>
2024-09-27 13:41:36 +0530

    Add files via upload

---
+++ car/script.js
@@ -0,0 +1,8 @@
+var pass = document.getElementById("show-pass-checkbox");
+pass.addEventListener("click",function(){
+    if(pass.checked){
+        document.getElementById("pass").type = "text"
+    }else{
+        document.getElementById("pass").type = "password"
+    }
+});
